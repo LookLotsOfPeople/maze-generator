dfde3d17f61a3555a511e44483b58a60bc7f965c
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-17 21:00:58 -0800

    Cell Update

--- src/main/kotlin/com/beyondbell/mazegenerator/Cell.kt
+++ src/main/kotlin/com/beyondbell/mazegenerator/Cell.kt
@@ -2,4 +2,9 @@ package com.beyondbell.mazegenerator
 
 class Cell {
 	val sections = ArrayList<Pair<Int, Int>>()
+
+	var topWall = true
+	var bottomWall = true
+	var leftWall = true
+	var rightWall = true
 }
--- src/main/kotlin/com/beyondbell/mazegenerator/Maze.kt
+++ src/main/kotlin/com/beyondbell/mazegenerator/Maze.kt
@@ -1,7 +1,7 @@
 package com.beyondbell.mazegenerator
 
 class Maze(dimension: Int, startPosition: Pair<Int, Int> = Pair(0, 0), endingPosition: Pair<Int, Int> = Pair(dimension - 1, dimension - 1)) {
-	private val grid = Array(dimension + 2) { Array(dimension + 2) { false } }
+	private val grid = Array(dimension + 2) { Array(dimension + 2) { Cell() } }
 
 	init {
 		generate()
